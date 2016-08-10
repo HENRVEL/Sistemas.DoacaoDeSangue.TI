b6d81136fe352e257911432ba9511d427170d4c1
HENRVEL <HENRIQUE@DESKTOP-SI44J2O>
2016-08-10 00:33:04 -0300

    Teste

---
+++ Sistemas.DoacaoDeSangue.Log/Logar.cs
@@ -0,0 +1,15 @@
+﻿using Sistemas.DoacaoDeSangue.DTO;
+using Sistemas.DoacaoDeSangue.DAO;
+
+namespace Sistemas.DoacaoDeSangue.Log
+{
+    public class Logar
+    {
+        BD_DOACAOEntities entities = new BD_DOACAOEntities();
+
+        public void CriaLog(ObjetoLog objetoLog)
+        {
+            entities.CriarLog(objetoLog.TipoLog, objetoLog.Mensagem);
+        }
+    }
+}
